c31421a5cf095bab8bffaa0c02e44b5fb6364078
guoxuesong <zenothing@hotmail.com>
2017-08-11 22:18:50 +0800

    googlenet support auxiliary classifier

--- deepstacks/modelzoo.py
+++ deepstacks/modelzoo.py
@@ -8,8 +8,9 @@ from macros import namespace
 from macros import ln
 from stacked import softmax
 
+from framework.main import newlayers,layers
 
-def googlenet(n):
+def googlenet(n,auxiliary_classifier=True):
     return macros((
         (0, 64, 7, 2, 0, 0, {'layername': 'conv1/7x7_s2'}),
         (0, 0, 3, 2, 0, 0, {'maxpool': True, 'layername': 'pool1/3x3_s2'}),
@@ -35,6 +36,20 @@ def googlenet(n):
             (inception, (64,   192,  96,   208,  16,  48,)),
             )),
         (ln,0,'inception_4a'),
+        (switch,auxiliary_classifier,(
+            (0, 0, 5, 3, 0, 0, {
+                'meanpool': True, 'pad': 0, 'layername': 'loss1/ave_pool'}),
+            (0, 128, 1, 1, 0, 0, {'layername': 'loss1/conv'}),
+            (0, 1024, 0, 0, 'loss1/fc', 0, {
+                'dense': True, 'layername': 'loss1/fc'}),
+            (0, 0, 0, 0, 0, 0, {
+                'dropout': 0.7, 'layername': 'loss1/dropout'}),
+            (0, n, 0, 0, 'loss1/classifier', 0, {
+                'dense': True, 'linear': True, 'layername': 'loss1/classifier'}),
+            (0, 0, 0, 0, 'loss1/prob', 0, {
+                'nonlinearity': (lambda x:x,softmax), 'layername': 'loss1/prob'}),
+            ),()),
+        (ln,'inception_4a'),
         (namespace, 'inception_4b/', (
             (inception, (64,   160,  112,  224,  24,  64,)),
             )),
@@ -47,6 +62,20 @@ def googlenet(n):
             (inception, (64,   112,  144,  288,  32,  64,)),
             )),
         (ln,0,'inception_4d'),
+        (switch,auxiliary_classifier,(
+            (0, 0, 5, 3, 0, 0, {
+                'meanpool': True, 'pad': 0, 'layername': 'loss2/ave_pool'}),
+            (0, 128, 1, 1, 0, 0, {'layername': 'loss2/conv'}),
+            (0, 1024, 0, 0, 'loss2/fc', 0, {
+                'dense': True, 'layername': 'loss2/fc'}),
+            (0, 0, 0, 0, 0, 0, {
+                'dropout': 0.7, 'layername': 'loss2/dropout'}),
+            (0, n, 0, 0, 'loss2/classifier', 0, {
+                'dense': True, 'linear': True, 'layername': 'loss2/classifier'}),
+            (0, 0, 0, 0, 'loss2/prob', 0, {
+                'nonlinearity': (lambda x:x,softmax), 'layername': 'loss2/prob'}),
+            ),()),
+        (ln,'inception_4d'),
         (namespace, 'inception_4e/', (
             (inception, (128,  256,  160,  320,  32,  128, )),
             )),
@@ -64,8 +93,8 @@ def googlenet(n):
             'meanpool': True, 'pad': 0, 'layername': 'pool5/7x7_s1'}),
         (0, 0, 0, 0, 0, 0, {
             'dropout': 0.7, 'layername': 'pool5/dropout'}),
-        (0, n, 0, 0, 0, 0, {
+        (0, n, 0, 0, 'loss3/classifier', 0, {
             'dense': True, 'linear': True, 'layername': 'loss3/classifier'}),
-        (0, 0, 0, 0, 0, 0, {
+        (0, 0, 0, 0, 'prob', 0, {
             'nonlinearity': (lambda x:x,softmax), 'layername': 'prob'}),
         ))
