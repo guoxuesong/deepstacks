f497b2725a3c7286b8f33abf27baa6df7ed0e5a2
guoxuesong <zenothing@hotmail.com>
2017-08-26 19:15:58 +0800

    remove __all__

--- deepstacks/__init__.py
+++ deepstacks/__init__.py
@@ -1,19 +1,19 @@
 from .stacked import curr_layer, curr_stacks, curr_flags, curr_model
 
-__all__ = []
+#__all__ = []
 
 try:
     import lasagne
 except ImportError:
     pass
-else:
-    __all__ += [lasagne]
+#else:
+#    __all__ += [lasagne]
 
 try:
     import neon
 except ImportError:
     pass
-else:
-    __all__ += [neon]
+#else:
+#    __all__ += [neon]
 
-__all__ += [curr_layer, curr_stacks, curr_flags, curr_model]
+#__all__ += [curr_layer, curr_stacks, curr_flags, curr_model]
