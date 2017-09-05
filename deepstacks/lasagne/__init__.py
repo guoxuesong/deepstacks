bcae7beef53456b0d34964d34e9b224f4766e7c5
guoxuesong <zenothing@hotmail.com>
2017-09-05 23:56:36 +0800

    add curr_filters,curr_width,curr_height,curr_depth

--- deepstacks/lasagne/__init__.py
+++ deepstacks/lasagne/__init__.py
@@ -1,8 +1,8 @@
 from .convdeconv import build_network
 from .convdeconv import curr_layer, curr_stacks, curr_flags, curr_model
 from .utils import get_loss
-from .api import curr_batchsize
+from .api import curr_batchsize,curr_filters,curr_width,curr_height,curr_depth
 
 __all__ = [
         build_network, curr_layer, curr_stacks, curr_flags,
-        curr_model, get_loss, curr_batchsize]
+        curr_model, get_loss, curr_batchsize,curr_filters,curr_width,curr_height,curr_depth]
