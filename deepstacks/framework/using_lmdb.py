2735def79a2d2989fc68a82468c5607697db9d96
guoxuesong <zenothing@hotmail.com>
2017-09-09 16:08:24 +0800

    fix shape bug,fix thread bug

--- deepstacks/framework/using_lmdb.py
+++ deepstacks/framework/using_lmdb.py
@@ -35,6 +35,8 @@ def iterate_minibatches(batchsize, database, shufflesize=1, use_caffe=True):
             s.write(datum.data)
             s.seek(0)
             img = np.array(PIL.Image.open(s))
+            if len(img.shape) == 2:
+                img = img[:,:,np.newaxis]
             img = img.transpose(2,0,1)
 
             label = datum.label
@@ -52,12 +54,12 @@ def iterate_minibatches(batchsize, database, shufflesize=1, use_caffe=True):
 
     assert n > batchsize
 
-    data_batches = np.zeros((batchsize, img_channels, img_width, img_height), dtype=theano.config.floatX)
-    labels_batches = np.zeros((batchsize,), dtype=np.int32)
-
     localshuffler = LocalShuffler(batchsize*shufflesize,(img_channels, img_width, img_height))
 
     for start_idx in range(0, n - batchsize + 1, batchsize):
+        data_batches = np.zeros((batchsize, img_channels, img_width, img_height), dtype=theano.config.floatX)
+        labels_batches = np.zeros((batchsize,), dtype=np.int32)
+
         try: 
             for i in xrange(batchsize):
                 (key, value) = cursor.item()
@@ -72,6 +74,8 @@ def iterate_minibatches(batchsize, database, shufflesize=1, use_caffe=True):
                     s.write(datum.data)
                     s.seek(0)
                     img = np.array(PIL.Image.open(s))
+                    if len(img.shape) == 2:
+                        img = img[:,:,np.newaxis]
                     img = img.transpose(2,0,1)
 
                     label = datum.label
@@ -87,35 +91,40 @@ def iterate_minibatches(batchsize, database, shufflesize=1, use_caffe=True):
             in_db_data.close()
             print "Unexpected error:", sys.exc_info()[0]
             raise 
-
-        if not localshuffler.is_full():
-            #print localshuffler.fill
-            for i in range(batchsize):
-                res=localshuffler.feed(data_batches[i],labels_batches[i])
-                assert res is None
-        else:
-            for i in range(batchsize):
-                x,y=localshuffler.feed(data_batches[i],labels_batches[i])
-                data_batches[i]=x
-                labels_batches[i]=y
-            X = {
-                    'image':data_batches,
-                    'target':labels_batches.astype('int64'),
-                    }
-            yield X
-    while not localshuffler.is_empty():
-        #print localshuffler.fill
-        data_batches=np.zeros_like(data_batches)
-        labels_batches=np.zeros_like(labels_batches)
-        for i in range(batchsize):
-            x,y=localshuffler.feed(None,None)
-            data_batches[i]=x
-            labels_batches[i]=y
         X = {
                 'image':data_batches,
                 'target':labels_batches.astype('int64'),
                 }
         yield X
+
+#        if not localshuffler.is_full():
+#            #print localshuffler.fill
+#            for i in range(batchsize):
+#                res=localshuffler.feed(data_batches[i],labels_batches[i],0)
+#                assert res is None
+#        else:
+#            for i in range(batchsize):
+#                x,y,ig=localshuffler.feed(data_batches[i],labels_batches[i],0)
+#                data_batches[i]=x
+#                labels_batches[i]=y
+#            X = {
+#                    'image':data_batches,
+#                    'target':labels_batches.astype('int64'),
+#                    }
+#            yield X
+#    while not localshuffler.is_empty():
+#        #print localshuffler.fill
+#        data_batches=np.zeros_like(data_batches)
+#        labels_batches=np.zeros_like(labels_batches)
+#        for i in range(batchsize):
+#            x,y,ig=localshuffler.feed(None,None,None)
+#            data_batches[i]=x
+#            labels_batches[i]=y
+#        X = {
+#                'image':data_batches,
+#                'target':labels_batches.astype('int64'),
+#                }
+#        yield X
 
 from scipy.misc import imsave,imread
 def iterate_one(batchsize, database):
@@ -127,6 +136,7 @@ def iterate_one(batchsize, database):
     yield X
 
 register_batch_iterator(
+        AsyncIterate(curry(iterate_minibatches,shufflesize=1)),
         AsyncIterate(curry(iterate_minibatches,shufflesize=1)),
         AsyncIterate(curry(iterate_minibatches,shufflesize=1)),
         AsyncIterate(curry(iterate_one)),
