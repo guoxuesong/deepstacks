4eab03cce1b814ed9fc3593114b36b7bde77eee5
guoxuesong <zenothing@hotmail.com>
2017-09-21 15:46:26 +0800

    remove buggy register_batches in iterate_minibatches;return ids;support minibatch_handlers;fix iterate_one bug

--- deepstacks/framework/using_lmdb.py
+++ deepstacks/framework/using_lmdb.py
@@ -2,23 +2,38 @@
 # coding:utf-8
 # vi:tabstop=4:shiftwidth=4:expandtab:sts=4
 
+#import cv2
 import sys
 import numpy as np
 import random
 import lmdb
 import caffe
 import theano
-from .main import register_batch_iterator
+from .main import register_batch_iterator,register_batches
 from ..utils.curry import curry
 from ..utils.async_iterate import AsyncIterate
 from StringIO import StringIO
 import PIL.Image
 from ..utils.localshuffler import LocalShuffler
 
+#from deepstacks.framework.main import register_batch_iterator,register_batches
+#from deepstacks.utils.curry import curry
+#from deepstacks.utils.async_iterate import AsyncIterate
+#from StringIO import StringIO
+#import PIL.Image
+#from deepstacks.utils.localshuffler import LocalShuffler
+
+minibatch_handlers=[]
+def register_minibatch_handler(h):
+    global minibatch_handlers
+    minibatch_handlers+=[h]
+
+
 def iterate_minibatches(batchsize, database, shufflesize=1, use_caffe=True):
 
     in_db_data = lmdb.open(database, readonly=True)
     n = int(in_db_data.stat()['entries'])
+    #register_batches(n//batchsize)
 
     with in_db_data.begin() as in_txn:
         cursor = in_txn.cursor()
@@ -51,6 +66,7 @@ def iterate_minibatches(batchsize, database, shufflesize=1, use_caffe=True):
     in_txn = in_db_data.begin()
     cursor = in_txn.cursor()
     cursor.first()
+    pos=0
 
     assert n > batchsize
 
@@ -59,6 +75,7 @@ def iterate_minibatches(batchsize, database, shufflesize=1, use_caffe=True):
     for start_idx in range(0, n - batchsize + 1, batchsize):
         data_batches = np.zeros((batchsize, img_channels, img_width, img_height), dtype=theano.config.floatX)
         labels_batches = np.zeros((batchsize,), dtype=np.int32)
+        ids_batches = np.zeros((batchsize,), dtype=np.int64)
 
         try: 
             for i in xrange(batchsize):
@@ -85,7 +102,9 @@ def iterate_minibatches(batchsize, database, shufflesize=1, use_caffe=True):
 
                 data_batches[i,:,:,:] = img
                 labels_batches[i] = np.int32(label)
+                ids_batches[i] = np.int64(pos)
                 cursor.next()
+                pos+=1
         except:
             cursor.close() 
             in_db_data.close()
@@ -95,7 +114,9 @@ def iterate_minibatches(batchsize, database, shufflesize=1, use_caffe=True):
                 'image':data_batches,
                 'target':labels_batches.astype('int64'),
                 }
-        yield X
+        for h in minibatch_handlers:
+            h(X,ids_batches)
+        yield X,ids_batches
 
 #        if not localshuffler.is_full():
 #            #print localshuffler.fill
@@ -128,25 +149,37 @@ def iterate_minibatches(batchsize, database, shufflesize=1, use_caffe=True):
 
 from scipy.misc import imsave,imread
 def iterate_one(batchsize, database):
-    image=imread(database).transpose(2,0,1)[np.newaxis,...]
+    #print 'iterate_one',batchsize,database
+    image=imread(database)
+    if len(image.shape)==2:
+        image=image[...,np.newaxis]
+    image=image.transpose(2,0,1)[np.newaxis,...]
+    ids = np.zeros((batchsize,),dtype='int64')
     X = {
             'image':image,
             'target':np.zeros((1,1),dtype=theano.config.floatX),
             }
-    yield X
+    #print X,ids
+    for h in minibatch_handlers:
+        h(X,ids)
+    #print X,ids
+    yield X,ids
 
 register_batch_iterator(
         AsyncIterate(curry(iterate_minibatches,shufflesize=1)),
         AsyncIterate(curry(iterate_minibatches,shufflesize=1)),
         AsyncIterate(curry(iterate_minibatches,shufflesize=1)),
         AsyncIterate(curry(iterate_one)),
+        #iterate_one,
         )
 
 if __name__=='__main__':
-    count = 0
-    for x in iterate_minibatches(512,'/home/ubuntu/work/DIGITS/digits/jobs/20170825-163329-a5a4/train_db',shufflesize=2):
-        print x['image'].shape
-        print x['target'].shape
-        print count
-        count += 1
+#    count = 0
+#    for x in iterate_minibatches(512,'/home/ubuntu/work/DIGITS/digits/jobs/20170825-163329-a5a4/train_db',shufflesize=2):
+#        print x['image'].shape
+#        print x['target'].shape
+#        print count
+#        count += 1
+    for x,ids in AsyncIterate(curry(iterate_one))(1,'gogopher.jpg'):
+        print x,ids
 
